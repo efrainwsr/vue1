87dd41cb9654069872f15db9916b74706f7a7bf2
efrainwsr <efrain.figueredowsr@gmail.com>
2023-05-16 00:06:53 -0400

    Intentando importar datos.js

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
   
- import Pokemon from './components/Pokemon.vue';
+ import Reyes from './components/Reyes.vue';
 
 </script>
   
 <template>
    <div class="container">
       <div class="row">
-         <div class="col col-sm-6"><Pokemon/></div>
-         <div class="col col-sm-6"><Pokemon/></div>
+         <div class="col col-sm-4"></div>
+         <div class="col col-sm-4"><Reyes/></div>
+         <div class="col col-sm-4"></div>
      </div>
      
    </div>
---
+++ src/components/Reyes.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import ref from 'vue';
+    import datos from '../datos.js';
+
+    
+
+</script>
+
+<template>
+<div class="card">
+  <div class="card-body text-center">
+    <h5 class="card-title">Título de la tarjeta</h5>
+    <p class="card-text">Este es un párrafo de ejemplo para la descripción de la tarjeta.</p>
+    <span class="card-text">Este es un ejemplo de un span dentro de la tarjeta.</span>
+    <img src="https://www.html6.es/img/rey_ervigio.png" class="card-img-top" alt="Imagen de la tarjeta">
+    <div class="text-center">
+      <a href="#" class="btn btn-primary">Botón {{  }}</a>
+    </div>
+  </div>
+</div>
+</template>
